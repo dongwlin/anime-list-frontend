be708905acfecf84f39e18bd6d8404e628123f4c
dongwlin <dongwlin@foxmail.com>
2024-09-25 02:16:47 +0800

    feat: 404 page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,14 @@
+import { useI18n } from '@/i18n'
 import { createWebHashHistory, createRouter } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    title?: string
+  }
+}
+
+const { t } = useI18n()
 
 const routes: RouteRecordRaw[] = [
   {
@@ -7,20 +16,45 @@ const routes: RouteRecordRaw[] = [
     redirect: '/home'
   },
   {
-    name: 'home',
     path: '/home',
+    name: 'home',
+    meta: {
+      title: t('page.home.title')
+    },
     component: () => import('@/views/home/index.vue')
   },
   {
-    name: 'settings',
     path: '/settings',
+    name: 'settings',
+    meta: {
+      title: t('page.settings.title')
+    },
     component: () => import('@/views/settings/index.vue')
+  },
+  {
+    path: '/404',
+    name: 'not-found',
+    meta: {
+      title: t('page.not_found.title')
+    },
+    component: () => import('@/views/not-found/index.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/404'
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, _, next) => {
+  const { title } = to.meta
+  if (title) document.title = title
+
+  next()
 })
 
 export default router
---
+++ src/views/not-found/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useI18n } from '@/i18n'
+import { NResult } from 'naive-ui'
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <n-result status="404" size="huge" :title="t('page.not_found.title')"
+    :description="t('page.not_found.description')"></n-result>
+</template>
+
+<style scoped></style>
